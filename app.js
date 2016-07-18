41d727abb1e873497117b511ed9a6fcbca12a2a1
Vasiliki Petychaki <vasilikipetyhaki@gmail.com>
2016-07-18 08:31:39 +0000

    deleted default function to test my own

--- app.js
+++ app.js
@@ -72,8 +72,3 @@ http.createServer(function(req, res){
 
 /////////////////////////////////////////
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
