d2a1c15642f14f726b92c9234991e27de3aefc66
Vasiliki Petychaki <vasilikipetyhaki@gmail.com>
2016-07-18 09:40:59 +0000

    trying something similar to "express_website" app

--- app.js
+++ app.js
@@ -7,6 +7,9 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -15,8 +18,15 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',function(req,res){
+	console.log('Hello World');
+	res.sendfile('Hello to you too');
+});
+
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
