859d7de01a8b570f5e73fa2f028938897491c820
Vasiliki Petychaki <vasilikipetyhaki@gmail.com>
2016-07-18 09:31:58 +0000

    didn't work. trying back to original form.

--- app.js
+++ app.js
@@ -6,69 +6,25 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-//var express = require('express');
+var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
 // create a new express server
-//var app = express();
+var app = express();
 
 // serve the files out of ./public as our main files
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
 
-//////////////////////////////////////////
-const http = require('http');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
 
-const mimeTypes = {
-	"html": "text/html",
-	"jpeg": "image/jpeg",
-	"jpg": "image/jpg",
-	"png": "image/png",
-	"js": "text/javascript",
-	"css": "text/css"
-};
-
-http.createServer(function(req, res){
-	var uri = url.parse(req.url).pathname;
-	var fileName = path.join(process.cwd(), unescape(uri));
-	console.log('Loading '+ uri);
-	var stats;
-
-	try{
-		stats = fs.lstatSync(fileName);
-	} catch(e){
-		res.writeHead(404, {'Content-type': 'text/plain'});
-		res.write('404 Not Found\n');
-		res.end();
-		return;
-	}
-
-	if(stats.isFile()){
-		var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
-		res.writeHead(200, {'Content-type': mimeType});
-
-		var fileStream = fs.createReadStream(fileName);
-		fileStream.pipe(res);
-	} else if(stats.isDirectory()) {
-		res.writeHead(302, {
-			'Location': 'index.html'
-		});
-		res.end();
-	} else {
-		res.writeHead(500, {'Content-type':'text/plain'});
-		res.write('500 Internal Error\n');
-		res.end();
-	}
-}).listen(3000);
-
-/////////////////////////////////////////
-
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
